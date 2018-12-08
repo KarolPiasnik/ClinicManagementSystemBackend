5654d64e6a766fb1f681d8b6d7d0b8c0803cc38c
Karol Piasnik <piasnik.karol@outlook.com>
2018-12-08 19:30:09 +0100

    add basic activation and registration endpoints

--- src/main/java/com/clinicmanagementsystem/cms/model/User.java
+++ src/main/java/com/clinicmanagementsystem/cms/model/User.java
@@ -1,25 +1,58 @@
 package com.clinicmanagementsystem.cms.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Document
 public class User extends Auditable {
-
+    @JsonSerialize(using = ToStringSerializer.class)
     @Id
     ObjectId id;
 
+    @NotNull
+    @NotBlank
     private String name;
 
+    @NotNull
+    @NotBlank
     private String surname;
 
     private  Person personalInfo;
 
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    private Boolean active;
+
+    @NotBlank
     private String pwd;
 
+    @NotBlank
+    @Indexed(unique = true)
     private String login;
 
+    @NotBlank
+    @Indexed(unique = true)
     private String email;
 
     public String getName() {
--- src/main/java/com/clinicmanagementsystem/cms/rest/controllers/RegisterController.java
+++ src/main/java/com/clinicmanagementsystem/cms/rest/controllers/RegisterController.java
@@ -1,7 +1,43 @@
 package com.clinicmanagementsystem.cms.rest.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.clinicmanagementsystem.cms.exceptions.AlreadyActivatedException;
+import com.clinicmanagementsystem.cms.model.User;
+import com.clinicmanagementsystem.cms.rest.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
 
 @RestController
 public class RegisterController {
+
+    @Autowired
+    UserRepository repository;
+
+    @PostMapping("register")
+    @ResponseBody
+    public User register(@Valid @RequestBody User user) {
+        user.setId(null);
+        user.setActive(Boolean.FALSE);
+        return repository.save(user);
+    }
+
+
+    @GetMapping("register/{id}/activate")
+    @ResponseBody
+    public User activate(@PathVariable("id") String id) {
+        User user = repository.findById(id).get();
+        if (user != null) {
+            if (!user.getActive()) {
+                user.setActive(Boolean.TRUE);
+            }
+        } else {
+            throw new DataRetrievalFailureException("The account that you try to activate no longer exists");
+        }
+        repository.save(user);
+        return user;
+    }
+
 }
