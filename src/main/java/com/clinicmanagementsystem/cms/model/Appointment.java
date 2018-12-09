704f116fcb7949b88b193100ccba5e8a000b2562
Karol Piasnik <piasnik.karol@outlook.com>
2018-12-09 17:25:10 +0100

    add doctor function

--- src/main/java/com/clinicmanagementsystem/cms/model/Appointment.java
+++ src/main/java/com/clinicmanagementsystem/cms/model/Appointment.java
@@ -1,4 +1,39 @@
 package com.clinicmanagementsystem.cms.model;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.util.Date;
+
 public class Appointment extends Auditable{
+    Date date;
+
+    @DBRef
+    Doctor doctor;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    @DBRef
+    Patient patient;
 }
